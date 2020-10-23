e225d3c1c58eac11a8a4026320a92b692db157fa
KEVIN LOPEZ MEZA <kevin.lopez3@ulatina.net>
2020-10-22 20:39:26 -0600

    Primer cambio prueba

--- Proyecto Repuestos/src/proyecto/repuestos/ProyectoRepuestos.java
+++ Proyecto Repuestos/src/proyecto/repuestos/ProyectoRepuestos.java
@@ -17,6 +17,9 @@ public class ProyectoRepuestos {
     public static void main(String[] args) {
         System.out.println("INICIAMOS");
         
+        
+         System.out.println("Cambio de Kevin");
+        
     }
     
 }
